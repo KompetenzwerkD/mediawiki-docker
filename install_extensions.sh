ce1b9efff5e5f8f67ad6700b23cff60d4212117d
muehleder <muehleder@saw-leipzig.de>
2021-02-19 13:50:04 +0100

    added title icon extension

--- install_extensions.sh
+++ install_extensions.sh
@@ -1,8 +1,11 @@
 cd extensions
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/PageSchemas.git 
-wget http://www.aifb.kit.edu/images/f/f3/SemanticMediaWikiGraph_v.1.0.1.zip
-unzip SemanticMediaWikiGraph_v.1.0.1.zip
 git clone https://github.com/wikimedia/mediawiki-extensions-PageForms
 mv mediawiki-extensions-PageForms PageForms
+wget http://www.aifb.kit.edu/images/f/f3/SemanticMediaWikiGraph_v.1.0.1.zip
+unzip SemanticMediaWikiGraph_v.1.0.1.zip
 rm SemanticMediaWikiGraph_v.1.0.1.zip
+wget https://extdist.wmflabs.org/dist/extensions/TitleIcon-REL1_35-2e9ce16.tar.gz
+tar -xzf TitleIcon-REL1_35-2e9ce16.tar.gz -C .
+rm  TitleIcon-REL1_35-2e9ce16.tar.gz
 cd ..
